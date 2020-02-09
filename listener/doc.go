91628900dbbc8078c07ec52fd6e25344c836a01c
Vaibhav Chellani <vaibhavchellani223@gmail.com>
2020-02-09 17:28:31 +0530

    listener for events

---
+++ listener/doc.go
@@ -0,0 +1,10 @@
+package listener
+
+/**
+Listener listens for events on various smart contracts and updates the DB state of the operator_address
+
+Use Cases :
+1. While syncing a new operator node
+2. Syncing deposits
+3. Confirming batches
+**/
