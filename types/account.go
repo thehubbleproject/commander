c9b3886d57db45a30bfb478cc6b68f1d30350f42
Vaibhav Chellani <vaibhavchellani223@gmail.com>
2020-03-21 15:15:30 +0530

    updated account type

--- types/account.go
+++ types/account.go
@@ -1,38 +1,47 @@
 package types
 
 import (
+	"math/big"
+
 	"github.com/BOPR/common"
 	"github.com/BOPR/contracts/rollup"
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"math/big"
 )
 
-type AccountLeaf struct {
-	Path      uint64
-	Balance   uint64
+// UserAccount is the user data stored on the node per user
+type UserAccount struct {
+	// ID is the path of the user account in the PDA Tree
+	// Cannot be changed once created
+	ID uint64
+	// Token type of the user account
+	// Cannot be changed after creation
 	TokenType uint64
-	Nonce     uint64
+
+	// Balance of the user account
+	Balance uint64
+	// Nonce of the account
+	Nonce uint64
 }
 
-func NewAccountLeaf(path, balance, tokenType, nonce uint64) AccountLeaf {
-	return AccountLeaf{
-		Path:      path,
+func NewUserAccount(id, balance, tokenType, nonce uint64) UserAccount {
+	return UserAccount{
+		ID:        id,
 		Balance:   balance,
 		TokenType: tokenType,
 		Nonce:     nonce,
 	}
 }
 
-func (acc *AccountLeaf) ToABIAccount() rollup.DataTypesAccount {
+func (acc *UserAccount) ToABIAccount() rollup.DataTypesAccount {
 	return rollup.DataTypesAccount{
-		Path:      UintToBigInt(acc.Path),
+		ID:        UintToBigInt(acc.id),
 		Balance:   UintToBigInt(acc.Balance),
 		TokenType: UintToBigInt(acc.TokenType),
 		Nonce:     UintToBigInt(acc.Nonce),
 	}
 }
 
-func (acc *AccountLeaf) ABIEncode() []byte {
+func (acc *UserAccount) ABIEncode() []byte {
 	uint256Ty, _ := abi.NewType("uint256", "uint256", nil)
 	arguments := abi.Arguments{
 		{
@@ -57,7 +66,7 @@ func (acc *AccountLeaf) ABIEncode() []byte {
 	return bytes
 }
 
-func AccsToLeafHashes(accs []AccountLeaf) (result [][32]byte) {
+func AccsToLeafHashes(accs []UserAccount) (result [][32]byte) {
 	for i, acc := range accs {
 		result[i] = BytesToByteArray(common.Hash(acc.ABIEncode()))
 	}
