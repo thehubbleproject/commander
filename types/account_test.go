6d2f61ace2bb497ee37309fbb5ad0dfa75b99027
Vaibhav Chellani <vaibhavchellani223@gmail.com>
2020-04-20 18:22:23 +0530

    added test case for zero leaf

---
+++ types/account_test.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/BOPR/common"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEmptyAccount(t *testing.T) {
+	// create an empty inactive account
+	acc, err := ABIEncode()
+	require.NoError(t, err)
+	require.Equal(t, ZERO_VALUE_LEAF.String(), common.Keccak256(acc).Hex()[2:], "The root leaves should match")
+}
+func ABIEncode() ([]byte, error) {
+	uint256Ty, err := abi.NewType("uint256", "uint256", nil)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	arguments := abi.Arguments{
+		{
+			Type: uint256Ty,
+		},
+	}
+	bytes, err := arguments.Pack(
+		big.NewInt(int64(0)),
+	)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return bytes, nil
+}
--- types/merkleTree.go
+++ types/merkleTree.go
@@ -8,10 +8,13 @@ import (
 )
 
 var defaultHashes []ByteArray
+
+// depicts the empty leaf in balance tree
 var ZERO_VALUE_LEAF ByteArray
 
 func init() {
 	var err error
+
 	ZERO_VALUE_LEAF, err = HexToByteArray("290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563")
 	if err != nil {
 		panic(err)
